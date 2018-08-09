2b57cca8304ead8c561b962d515946759b248ab9
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-08-09 20:52:41 +0300

    Add section calendar, add events calendar shortcode

--- tgm/connect.php
+++ tgm/connect.php
@@ -133,6 +133,18 @@ function visotskiy_register_required_plugins() {
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
 
+		array(
+			'name'               => 'The Events Calendar', // The plugin name.
+			'slug'               => 'the-events-calendar', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/tgm/plugins/the-events-calendar.4.6.21.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '4.6.21', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+		),
+
 	);
 
 	/*
