5750b89066eff9cf58d4e128e03e8b74d35590aa
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-08-10 15:59:32 +0300

    Calendar crash

--- template-home.php
+++ template-home.php
@@ -8,4 +8,9 @@ $post = new TimberPost();
 
 $context['post'] = $post;
 
+$calendar_events_json = json_encode(get_field('calendar_events'));
+//$calendar_events_json = preg_replace('/"([^"]+)"\s*:\s*/', '$1:', $calendar_events_json);
+
+$context['calendar_events'] = $calendar_events_json;
+
 Timber::render( array( 'template-home.twig' ), $context );
